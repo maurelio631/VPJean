999b3b534e44998e9ec6cb7ccfe9995b654f5e5c
Marco <marco.silva631@outlook.com>
2022-11-01 00:26:42 -0300

    Remoção de assets desnecessários e ajustes css

--- js/script.js
+++ js/script.js
@@ -1,8 +1,12 @@
 const TEMPO = 60000;
 const content = document.querySelector("#hiddenContent");
+const disclosure = document.querySelector("#hiddenDisclosure");
+const buyBtn = document.querySelector("#buyButton")
 
 function showContent(){
     content.classList.remove("d-none");
+    buyBtn.classList.remove("d-none");
+    disclosure.classList.add("d-none");
 }
 
 setTimeout(showContent,TEMPO);
